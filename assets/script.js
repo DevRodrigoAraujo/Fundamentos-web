72e2a35847f479a1b83b6304a9684be01e3441d7
Rodrigo Araujo <rodhtml0@gmail.com>
2023-04-26 10:04:55 -0300

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -1,127 +1,86 @@
-<<<<<<< HEAD
-=======
+let nome = document.getElementById("nome");
+let email = document.querySelector("#email");
+let assunto = document.querySelector("#assunto");
+let nomeOk = false;
+let emailOk = false;
+let assuntoOk = false;
+let mapa = document.querySelector("#mapa");
+let divMapa = document.querySelector("#div-mapa");
+
+nome.style.width = '100%';
+email.style.width = '100%';
+assunto.style.width = '100%';
 
 
->>>>>>> javascript
-let nome = document.getElementById('nome');
-let email = window.document.querySelector('#email');
-let assunto = document.querySelector('#assunto');
-let emailOk = false
-let assuntoOk = false
-let nomeOk = false
-let mapa = document.querySelector('#mapa')
-
-<<<<<<< HEAD
-function validaNome() {
-    let txtNome = document.querySelector("#txtNome")
-    if (nome.value.length <= 2) {
-
-        txtNome.innerHTML = 'Nome inválido'
-        txtNome.style.color = 'red'
-    } else {
-=======
-function validaNome () { 
-    let txtNome = document.querySelector("#txtNome")
-    if (nome.value.length <= 2 ) {
-        alert('erro')
-        txtNome.innerHTML = 'Nome inválido'
-        txtNome.style.color = 'red'
+function validarNome(){
+    let txtNome = document.querySelector("#txtNome");
+    if (nome.value.length < 3) {
+      /* InnerHTML add um valor a um elemento 
+       no caso, add a DIV txtNome um valor de texto, isso por meio da variavel let txt*/
+      txtNome.innerHTML = "Nome Inválido!";
+      txtNome.style.color = 'red';
     }
-    else {
->>>>>>> javascript
-        txtNome.innerHTML = 'Nome válido'
-        txtNome.style.color = 'green'
-        nomeOk = true
+    else{
+        txtNome.innerHTML = "Nome Válido!"
+        txtNome.style.color ='green';
+        nomeOk = true;
     }
 }
-<<<<<<< HEAD
 
-function validaEmail() {
-    let txtEmail = document.querySelector("#txtEmail")
-
+function validarEmail(){
+    let txtEmail = document.querySelector('#txtEmail');
     if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1) {
-
-        txtEmail.innerHTML = 'E-mail inválido'
-        txtEmail.style.color = 'red'
-    } else {
-=======
-function validaEmail() {
-    let txtEmail = document.querySelector("#txtEmail")
-
-    if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == - 1) {
-        alert('erro')
-        txtEmail.innerHTML = 'E-mail inválido'
-        txtEmail.style.color = 'red'
+        txtEmail.innerHTML = "E-mail Inválido!"
+        txtEmail.style.color = "red";
     }
-    else {
->>>>>>> javascript
-        txtEmail.innerHTML = 'Email válido'
-        txtEmail.style.color = 'green'
-        emailOk = true
+    else{
+        txtEmail.innerHTML = "E-mail Válido!"
+        txtEmail.style.color = "green";
+        emailOk = true;
     }
-<<<<<<< HEAD
-
+    
 }
 
-function validaAssunto() {
-    let txtAssunto = document.querySelector("#txtAssunto")
-
-    if (assunto.value.length >= 100) {
-
-        txtAssunto.innerHTML = 'Tamanho muito grande'
-        txtAssunto.style.color = 'red'
-        assuntoOk = false
-    } else {
-
-=======
-  
-}
-function validaAssunto() {
-    let txtAssunto = documento.querySelector('#txtAssunto')
-
-    if(assunto.value.length > 100) {
-        alert('assunto muito grande')
-        txtAssunto.innerHTML = 'Tamanho muito grande'
-        txtAssunto.style.color = 'red'
-    }else {
-        txtAssunto.innerHTML = 'Tamanho ok'
-        txtAssunto.style.color  = 'green'
->>>>>>> javascript
-        txtAssunto.style.display = 'none'
-        assuntoOk = true
+function validarAssunto(){
+    let txtAssunto = document.querySelector('#txtAssunto');
+    if (assunto.value.length >= 100 ) {
+        txtAssunto.innerHTML = "O assunto deve ter no máximo 100 caracteres!"
+        txtAssunto.style.color = 'red';
+        /*
+        como o 'Assunto' já começa com menos que 100 caracteres, o código já pula para o else e o display fica 'none' -
+        então add no if tbm uma configuração para que cair nele,  display fique block e msg apareça.
+        */
+        txtAssunto.style.display = 'block';
+        assuntoOk = false;
     }
-}
-<<<<<<< HEAD
-
-function enviar() {
-    if (nomeOk == true && emailOk == true && assuntoOk == true) {
-        alert('Formulário enviado com sucesso')
-    } else {
-        alert('Preencha o formulário antes de enviar')
+    else{
+        /*
+        display é como um elemento aparece na minha página.
+        quando add 'none' queremos que não apareça.
+        */        
+        txtAssunto.style.display = 'none';
+        assuntoOk = true;
     }
 }
 
-function mapaZoom() {
-=======
-function enviar () {
-    if ( nomeOk == true && emailOk == true && assuntoOk == true ) {
-    alert('Formulário enviado com sucesso')
-} else {
-    alert('Preencha o formulário antes de enviar')
-}
-}
-
-function mapaZoom () {
->>>>>>> javascript
-    mapa.style.width = '800px'
-    mapa.style.height = '400px'
+function enviar(){
+    if (nomeOk == true && assuntoOk == true && emailOk == true) {
+        alert("Formulário enviado!")
+    }
+    else{
+        alert("Formulário não foi enviado! Por favor, revise se os campos foram preenchidos corretamente.")
+    }
 }
 
-<<<<<<< HEAD
-function mapaNormal() {
-=======
-function mapaNormal () {
->>>>>>> javascript
-    mapa.style.width = '350px'
-    mapa.style.height = '250px'
+function mapaNormal(){
+    mapa.style.width = '400px';
+    mapa.style.height = '375px';
+    divMapa.style.width = '400px'
 }
+
+function mapaZoom(){
+    mapa.style.width = '800px';
+    mapa.style.height = '600px';
+    divMapa.style.width = '800px';
+}
+
